0dc0dceaf68701fdbf103f19772b8bc8e96d2546
yurivfernandes1 <yurivf@outlook.com>
2025-03-29 19:35:49 -0300

    Refatoração da classe AllCommit e remoção da classe GrokReport; atualização das dependências no requirements.txt

--- all_commit_dataset.py
+++ all_commit_dataset.py
@@ -1,40 +1,28 @@
 import httpx
 
-
 class AllCommit:
-    def __init__(
-        self,
-        user_id: int,
-        git_token: str,
-        empresa_url: str,
-        plataforma: str,
-        project_id: str,
-    ):
-        self.header = {"Private-Token": git_token}
+    def __init__(self, user_id: int, repo_name: str, owner: str, git_token: str, plataforma: str):
         self.user_id = user_id
-        self.url = ""
+        self.header = {"Private-Token": git_token} if plataforma == "GitLab" else {"Authorization": f"token {git_token}"}
         if plataforma == "GitLab":
-            self.url = f"https://gitlab{empresa_url}.com.br/api/v4/projects/{project_id}/repository/commits"
+            base_url = "https://gitlab.com/api/v4/projects"
+            self.url = f"{base_url}/{repo_name}/repository/commits" if not owner else f"https://gitlab.{owner}.com.br/api/v4/projects/{repo_name}/repository/commits"
         else:
-            self.url = f"https://github{empresa_url}.com.br/api/v4/projects/{project_id}/repository/commits"
+            base_url = "https://api.github.com/repos"
+            self.url = f"{base_url}/{owner}/{repo_name}/commits"
 
     @property
     def dataset(self) -> dict:
-        """Retorna um dicionário com a lista de commits do projeto filtrados pelo usuário e ordenados por id"""
+        """Retorna um dataset com a lista de commits do projeto filtrados pelo usuário"""
         try:
             response = httpx.get(
                 url=self.url,
                 headers=self.header,
-                params={"author_id": self.user_id, "ref_name": "homolog"},
+                params={"author": self.user_id, "per_page": 100},
             )
 
             if response.status_code == 200:
-                commits = response.json()
-                commits_list = [
-                    {"id": commit["id"], "title": commit["title"]}
-                    for commit in commits
-                ]
-                return sorted(commits_list, key=lambda x: x["id"])
+                return response.json()
             else:
                 raise ValueError(
                     f"Erro na requisição: {response.status_code} - {response.text}"
